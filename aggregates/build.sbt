604fc6d9e7f0514873d23f3b93a39cf33a2df039
Antoine Latrille <antoine.latrille@gmail.com>
2018-01-30 20:32:17 +0100

    Initial commit with project

---
+++ aggregates/build.sbt
@@ -0,0 +1,7 @@
+name := "aggregates"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+mainClass := Some("com.latrilla.Main")
---
+++ aggregates/src/main/scala/com/latrilla/Main.scala
@@ -0,0 +1,9 @@
+package com.latrilla
+
+object Main extends App {
+
+  override def main(args: Array[String]): Unit = {
+    var st:String = "Hello " + args.toList
+    println(st)
+  }
+}
